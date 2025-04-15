213d3a02705160f51293f004c11a4da08935edc7
prajinkhatiwada <prajinkhatiwada@gmail.com>
2025-04-15 19:10:20 +0545

    Fixed bugs

--- dice/lib/widgets/parallax_background.dart
+++ dice/lib/widgets/parallax_background.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ParallaxBackground extends StatefulWidget {
-  const ParallaxBackground({Key? key}) : super(key: key);
+  const ParallaxBackground({super.key});
 
   @override
   _ParallaxBackgroundState createState() => _ParallaxBackgroundState();
